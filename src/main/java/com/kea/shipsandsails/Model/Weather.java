63fcae4c01f4a51e0f648969ede9e0497700183c
andreasdan <andreas.petersen01@gmail.com>
2019-04-16 13:31:27 +0200

    Add weather

---
+++ src/main/java/com/kea/shipsandsails/Model/Weather.java
@@ -0,0 +1,30 @@
+package com.kea.shipsandsails.Model;
+
+public class Weather {
+    private Direction windDirection; //ENUM N, NE, SE, S, SW, NW
+    private int windSpeed; //0 - 5
+
+    public Weather() {
+    }
+
+    public Weather(Direction windDirection, int windSpeed) {
+        this.windDirection = windDirection;
+        this.windSpeed = windSpeed;
+    }
+
+    public Direction getWindDirection() {
+        return windDirection;
+    }
+
+    public void setWindDirection(Direction windDirection) {
+        this.windDirection = windDirection;
+    }
+
+    public int getWindSpeed() {
+        return windSpeed;
+    }
+
+    public void setWindSpeed(int windSpeed) {
+        this.windSpeed = windSpeed;
+    }
+}
