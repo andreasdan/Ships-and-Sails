429d5ea41678eb78a68b20ca779f00a002656ac5
andreasdan <andreas.petersen01@gmail.com>
2019-04-16 17:26:56 +0200

    Update Coordinate

--- src/main/java/com/kea/shipsandsails/Model/Coordinate.java
+++ src/main/java/com/kea/shipsandsails/Model/Coordinate.java
@@ -4,6 +4,9 @@ public class Coordinate {
     private int x;
     private int y;
 
+    public Coordinate() {
+    }
+
     public Coordinate(int x, int y) {
         this.x = x;
         this.y = y;
