704815974c4ea8f8422e3725a9a3dd4998b94bcd
andreasdan <andreas.petersen01@gmail.com>
2019-04-21 10:39:49 +0200

    Add IDs to Direction

--- src/main/java/com/kea/shipsandsails/Model/Direction.java
+++ src/main/java/com/kea/shipsandsails/Model/Direction.java
@@ -1,5 +1,23 @@
 package com.kea.shipsandsails.Model;
 
-public enum Direction {
-    N, NE, SE, S, SW, NW
+public enum Direction
+{
+    N(0),
+    NE(1),
+    SE(2),
+    S(3),
+    SW(4),
+    NW(5);
+
+    private int id;
+
+    Direction(int id)
+    {
+        this.id = id;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
 }
