227d3f6151b8f536667aa9dd4bc6f3002f983c8d
dani153i <danielblom90@gmail.com>
2019-04-25 04:07:00 +0200

    Added  * communication standalone module.  * communication service  * weather service
    Changes
     * commented out some code that needed further implementation, so the application is runnable

--- src/main/java/com/kea/shipsandsails/ShipsandsailsApplication.java
+++ src/main/java/com/kea/shipsandsails/ShipsandsailsApplication.java
@@ -1,5 +1,6 @@
 package com.kea.shipsandsails;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,8 +8,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShipsandsailsApplication {
 
     public static void main(String[] args) {
-
-
 
         SpringApplication.run(ShipsandsailsApplication.class, args);
     }
